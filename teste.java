e3d20f6a8c9283c9336e5062ab537cc09ea9aa1a
carlotasgs <97111744+carlotasgs@users.noreply.github.com>
2022-01-04 15:44:42 +0000

    Adicionei a soma
    Tem um erro

---
+++ teste.java
@@ -0,0 +1,33 @@
+public class Teste {
+
+	public static void main(String[]args) {
+		
+		System.out.println("Somar dois números"); //Título
+			
+		Scanner in = new Scanner(System.in); //Abrir caminho
+			
+		System.out.println("Escreva o primeiro número: "); //Pedido
+		int primeiroNumero = in.nextInt(); //Como vou ler/guardar
+		
+		System.out.println("Escreva o segundo número: ");
+		int segundoNumero = in.nextInt();
+		
+		int resultado = soma(primeiroNumero, segundoNumero);
+		
+		
+		System.out.println("A soma é: " + resultado);
+		
+		in.close();
+		
+		
+		public static int soma(int aPrimeiroNumer, int aSegundoNumero) {
+
+			int resultado = aPrimeiroNumero + aSegundoNumero;
+
+			}
+
+			return resultado;
+		
+	}
+		
+}
